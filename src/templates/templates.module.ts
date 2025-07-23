ffaa18f4c3fc9ffccfeeea84d37571e4c929a806
Idan Rossman <idanr@omnisys.co.il>
2025-07-23 15:23:58 +0300

    introduced template service

--- src/templates/templates.module.ts
+++ src/templates/templates.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TemplatesController } from './templates.controller';
+import { TemplateService } from './template.service';
 import { DatabaseModule } from '../database/database.module';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [TemplatesController],
+  providers: [TemplateService],
+  exports: [TemplateService],
 })
 export class TemplatesModule {}
